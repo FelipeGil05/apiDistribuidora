bd84898c1e90019b6853b77ecdc260adf7d855ec
Felipe Gil <felipeegil05@gmail.com>
2025-04-16 14:17:04 -0300

    Creacion de las Clases

---
+++ src/Domain/Entities/Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public bool Delivery { get; set; }
+        public List<Item>? Products { get; set; } = new List<Item>();
+        private const decimal Delivery_Pct = 0.10m;
+
+        public decimal TotalAmount
+        {
+            get
+            {
+                decimal total = Products?.Sum(d => d.Total) ?? 0;
+                return Delivery ? total + (total * Delivery_Pct) : total;
+            }
+        }
+    }
+}
---
+++ src/Domain/Entities/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total => UnitPrice + Quantity;
+        public int? CartId { get; set; }
+        [JsonIgnore]
+        public Cart? Cart { get; set; }
+    }
+}
